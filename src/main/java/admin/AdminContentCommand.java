332402534732a980a8fcd14d5a708a63fe14e6b5
eunhaKim <ara419@naver.com>
2024-05-14 17:53:33 +0900

    2024-05-14 17:53

--- src/main/java/admin/AdminContentCommand.java
+++ src/main/java/admin/AdminContentCommand.java
@@ -7,22 +7,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import member.MemberVO;
-
 public class AdminContentCommand implements AdminInterface {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		AdminDAO dao = new AdminDAO();
-		ArrayList<MemberVO>	vos = null;
 		
-		// 신규회원(준회원)
-		vos	= dao.getMemberList(1);
-		request.setAttribute("newMemberCnt", vos.size());
+		int mCount = dao.getNewMemberListCount();
 		
-		// 탈퇴회원
-		vos	= dao.getMemberList(99);
-		request.setAttribute("delMemberCnt", vos.size());
+		request.setAttribute("mCount", mCount);
 	}
 
 }
--- src/main/webapp/WEB-INF/admin/adminContent.jsp
+++ src/main/webapp/WEB-INF/admin/adminContent.jsp
@@ -22,8 +22,8 @@
   -->
   <p>방명록 새글 : ???</p>
   <p>게시글 새글 : ???</p>
-  <p>신규등록회원 : <a href="MemberList.ad?level=1"><b>${newMemberCnt}</b></a>건</p>
-  <p>탈퇴신청회원 : <a href="MemberList.ad?level=99"><b>${delMemberCnt}</b></a>건</p>
+  <p>신규등록회원 : <a href="MemberList.ad?level=1"><b>${mCount}</b></a>건</p>
+  <p>탈퇴신청회원 : <a href=""><b>${m99Count}</b></a>건</p>
 </div>
 <p><br/></p>
 </body>
