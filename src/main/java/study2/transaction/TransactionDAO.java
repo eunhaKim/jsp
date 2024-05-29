4bdc0751b2a9578af6c2a7000e67e276ad4f00a4
eunhaKim <ara419@naver.com>
2024-05-29 18:12:32 +0900

    2024-05-29 18:12

--- src/main/java/study2/transaction/TransactionDAO.java
+++ src/main/java/study2/transaction/TransactionDAO.java
@@ -1,4 +1,4 @@
-package study2;
+package study2.transaction;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,16 +7,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import study2.transaction.BankBookVO;
+import common.GetConn;
 
-public class StudyDAO {
+public class TransactionDAO {
+	/*
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
 	
 	String sql = "";
 	
-	public StudyDAO() {
+	public TransactionDAO() {
 		String driver = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/javaclass";
 		String user = "root";
@@ -31,12 +32,12 @@ public class StudyDAO {
 			System.out.println("DB연동 실패~" + e.getMessage());
 		}
 	}
+	*/
+	private Connection conn = GetConn.getConn();
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
 	
-	public void connClose() {
-		try {
-			if(conn != null) conn.close();
-		} catch (Exception e) {}
-	}
+	private String sql = "";
 	
 	public void pstmtClose() {
 		try {
@@ -49,24 +50,6 @@ public class StudyDAO {
 			if(rs != null) rs.close();
 			pstmtClose();
 		} catch (Exception e) {}
-	}
-
-	// hoewon테이블에서 아이디 검색후 성명 반환하기
-	public String getIdSearch(String mid) {
-		String name = "";
-		try {
-			sql = "select name from hoewon where mid = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, mid);
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) name = rs.getString("name");
-		} catch (SQLException e) {
-			System.out.println("SQL 오류 : " + e.getMessage());
-		} finally {
-			rsClose();
-		}
-		return name;
 	}
 
 	// BankBook정보 가져오기
@@ -96,7 +79,7 @@ public class StudyDAO {
 	public BankBookVO getBankBookMidSearch(String mid) {
 		BankBookVO vo = new BankBookVO();
 		try {
-			sql = "select * from bankBook where mid = ?";
+			sql = "select * from bankBook where mid = ? order by idx desc limit 1";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, mid);
 			rs = pstmt.executeQuery();
@@ -116,6 +99,9 @@ public class StudyDAO {
   // BankBookHistory에 사용내역 저장하기
 	public void setBankBookHistoryInput(BankBookVO vo) {
 		try {
+			// 트랜잭설정 : false를 인자값으로 설정하여 수동커밋으로 지정한다.
+			conn.setAutoCommit(false);
+			
 			sql = "insert into bankBookHistory values (default, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, vo.getIdx());
@@ -123,21 +109,38 @@ public class StudyDAO {
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("SQL 오류 : " + e.getMessage());
+			try {
+				if(conn != null) conn.rollback();	// 예외오류 발생시는 기존에 작업된 sql문의 모두 rollback처리된다.
+			} catch (Exception e2) {}
 		} finally {
 			pstmtClose();
 		}
 	}
 
   // 실제 잔고에 적용하기
-	public void setBankBookInput(BankBookVO vo) {
+	public void setBankBookInput(BankBookVO vo, String err) {
 		try {
 			sql = "insert into bankBook values (default, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, vo.getMid());
 			pstmt.setInt(2, vo.getBalance());
 			pstmt.executeUpdate();
+			
+	    if(err != null && !err.equals("")){
+	    	try {
+	        throw new Exception("진행 오류 발생 !!");
+	    	} catch (Exception e) {
+	    		if(conn != null) conn.rollback();
+	    	}
+	    }
+			
+			// 정상적으로 트랜잭션작업단위가 종료된후에 트랜잭션을 커밋시킨다.
+			conn.commit();
 		} catch (SQLException e) {
 			System.out.println("SQL 오류 : " + e.getMessage());
+			try {
+				if(conn != null) conn.rollback();	// 예외오류 발생시는 기존에 작업된 sql문의 모두 rollback처리된다.
+			} catch (Exception e2) {}
 		} finally {
 			pstmtClose();
 		}
